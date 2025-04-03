9bebc0a3c53452122d8a21be1ababe10062524b5
icode789 <rx.immnop@gmail.com>
2025-04-03 11:59:23 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,51 +1,63 @@
-<li className="nav-item active dropdown">
-  <a
-    href="https://www.google.com"  // Placeholder link for now
-    className="nav-link dropdown-toggle"
-    id="navbarDropdown"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-    WHO WE ARE
-  </a>
-  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-    <li><a href="https://www.google.com" className="dropdown-item">About Us</a></li>
-    <hr />
-    <li><a href="https://www.google.com" className="dropdown-item">Project Timeline</a></li>
-  </ul>
-</li>
+import React from 'react';
+import './App.css'; // Assuming you have your CSS in the App.css file
 
-<li className="nav-item active">
-  <a href="https://www.google.com" className="nav-link">DESIGN CODE</a>
-</li>
-<li className="nav-item active">
-  <a href="https://www.google.com" className="nav-link">OUR DISTRICTS</a>
-</li>
+function App() {
+  return (
+    <div>
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <img
+          className="d-inline-block align-top"
+          src="https://sportsboulevard.sa/images/Logo.svg"
+          alt="Logo"
+        />
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active dropdown">
+              <a
+                href="https://www.google.com" // Placeholder link for now
+                className="nav-link dropdown-toggle"
+                id="navbarDropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                WHO WE ARE
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a href="https://www.google.com" className="dropdown-item">
+                    About Us
+                  </a>
+                </li>
+                <hr />
+                <li>
+                  <a href="https://www.google.com" className="dropdown-item">
+                    Project Timeline
+                  </a>
+                </li>
+              </ul>
+            </li>
 
-<li className="nav-item active dropdown">
-  <a
-    href="https://www.google.com"  // Placeholder link for now
-    className="nav-link dropdown-toggle"
-    id="navbarDropdown"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-    MEDIA CENTER
-  </a>
-  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-    <li><a href="https://www.google.com" className="dropdown-item">News</a></li>
-    <hr />
-    <li><a href="https://www.google.com" className="dropdown-item">Media Gallery</a></li>
-    <li><a href="https://www.google.com" className="dropdown-item">Contact Media Center</a></li>
-    <li><a href="https://www.google.com" className="dropdown-item">Community Information Center</a></li>
-  </ul>
-</li>
-<li className="nav-item active">
-  <a href="https://www.google.com" className="nav-link">VENDOR RELATIONS</a>
-</li>
-<li className="nav-item active">
-  <a href="https://www.google.com" className="nav-link">CAREERS</a>
-</li>
-<li className="nav-item active">
-  <a href="https://www.google.com" className="nav-link">CONTACT US</a>
-</li>
+            <li className="nav-item active">
+              <a href="https://www.google.com" className="nav-link">
+                DESIGN CODE
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a href="https://www.google.com" className="nav-link">
+                OUR DISTRICTS
+              </a>
+            </li>
+
+            <li className="nav-item active dropdown">
+              <a
+                href="https://www.google.com" // Placeholder link for now
+                className="nav-link dropdown-toggle"
+                id="navbarDropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                MEDIA CENTER
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li
